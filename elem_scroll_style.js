8a08011ae40e99c0d389faf95c309ee2ae50da08
shuk <biz.shuk@gmail.com>
2015-05-28 15:52:14 +0800

    update

--- elem_scroll_style.js
+++ elem_scroll_style.js
@@ -1,12 +1,15 @@
 
-
-// 傳入 elem id , 和選擇的 scroll type , now only one scroll_type
+// Need: jquery 1.9.1+ 
+// 傳入 elem id , 選擇的 scroll type , now only one scroll_type
 function elem_fixed_on_top(elem_id,scroll_type){
+
     var last_elem_top = 0;
     var last_scroll_top = 0;
     var dmdc = 0; // diff move distance count
 
     var elem = $('#'+elem_id);
+    elem.addClass('elem_scroll_style1');    // init class
+
     var elem_height = elem.height();
     var original_elem_top = elem.position().top;
     var original_elem_bottom = original_elem_top + elem_height;
@@ -15,17 +18,18 @@ function elem_fixed_on_top(elem_id,scroll_type){
     // simular to  android chrome mobile app's url bar , unlimited by initial elem.top==0
     function elem_fixed_1(event){
         var doc_top = $(document).scrollTop();
+
         var diff_move_distance = last_scroll_top - doc_top;
-        var elem_top = parseInt(elem.position().top);
-        // if 46 已上position relative  已下  用diff看要不要跳
         dmdc += diff_move_distance;
+
+        var elem_top = parseInt(elem.position().top);
 
         // scroll 小於原本的位置bottom 
         if ( doc_top <= elem_height + original_elem_top ) {
             // [有點難解釋]捲軸高度 +???  小於 elem 原位置底部
             if ( doc_top + elem_height + elem_top <= original_elem_bottom ) {
                 elem.css({'top':'0px'});
-                elem.removeClass('t1');
+                elem.removeClass('elem_scroll_style1_on');
             }
 
         } else {
@@ -33,17 +37,17 @@ function elem_fixed_on_top(elem_id,scroll_type){
             // if relative => elem不會再隱藏位置 , only fixed and hidden will equal -elem_height
             if ( doc_top <= original_elem_bottom + placeholder && elem_top == -elem_height ){
                 elem.css({'top': '0px'});
-                elem.removeClass('t1');
+                elem.removeClass('elem_scroll_style1_on');
                 return;
             }
 
             {   // relative
-                if ( !elem.hasClass('t1') ){ // 變fixed前 預先移動到螢幕外
+                if ( !elem.hasClass('elem_scroll_style1_on') ){ // 變fixed前 預先移動到螢幕外
                     elem.css({'top': (-elem_height)+'px'});
                 }
 
                 if ( elem_top == -elem_height + original_elem_top ){ //fixed預先移動完成 即加上fixed
-                    elem.addClass('t1');
+                    elem.addClass('elem_scroll_style1_on');
                 }
             }
 
