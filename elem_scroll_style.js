04bfb0b591055cde205249c69d0dce8bcd0f00cd
shuk <biz.shuk@gmail.com>
2015-05-31 23:53:40 +0800

    tmp

--- elem_scroll_style.js
+++ elem_scroll_style.js
@@ -1,4 +1,3 @@
-
 /*  
     Required: jquery 1.9.1+  
     ******* important ******** 
@@ -7,6 +6,10 @@
     parameters:
         elem_id: a string for id of elem
         scroll_type: a number , only one scroll_type now
+
+
+    bug:
+        remove "on" class 時 top沒歸到0px ???
 */
 function elem_fixed_on_top(elem_id,scroll_type){
 
@@ -18,52 +21,65 @@ function elem_fixed_on_top(elem_id,scroll_type){
     elem.addClass('elem_scroll_style1');    // init class
 
     var elem_height = elem.outerHeight();
-    var original_elem_top = elem.position().top;
-    var original_elem_bottom = original_elem_top + elem_height;
+    var ori_elem_top = elem.position().top;
+    var ori_elem_bottom = ori_elem_top + elem_height;
     var placeholder = elem_height*2;
 
     // simular to  android chrome mobile app's url bar , unlimited by initial elem.top==0
     function elem_fixed_1(event){
         var doc_top = $(document).scrollTop();
 
-        var diff_move_distance = last_scroll_top - doc_top;
-        dmdc += diff_move_distance;
+        var move_distance = last_scroll_top - doc_top;
+        dmdc += move_distance;
 
+        // relative時 css top:0  其position().top = 46
+        // fixed時    css top:0  其position().top = 0
         var elem_top = parseInt(elem.position().top);
 
         // scroll 小於原本的位置bottom 
-        if ( doc_top <= elem_height + original_elem_top ) {
+        console.log(doc_top,ori_elem_bottom);
+        if ( doc_top <= ori_elem_bottom ) {
+            console.log(1);
             // [有點難解釋]捲軸高度 +???  小於 elem 原位置底部
-            if ( doc_top + elem_height + elem_top <= original_elem_bottom ) {
+            if ( doc_top + elem_height + elem_top <= ori_elem_bottom ) {
+                console.log("set default");
                 elem.css({'top':'0px'});
                 elem.removeClass('elem_scroll_style1_on');
             }
 
-        } else {
-            // scroll up 超過準備區 且 elem 尚未出現螢幕 , 
-            // if relative => elem不會再隱藏位置 , only fixed and hidden will equal -elem_height
-            if ( doc_top <= original_elem_bottom + placeholder && elem_top == -elem_height ){
-                elem.css({'top': '0px'});
+        } else {    // bug css top vs position top混用錯誤
+            // scroll up 超過準備區 且 elem 尚未出現螢幕 , if relative => elem不會再隱藏位置 ,  only fixed and hidden will equal -elem_height
+            if ( move_distance > 0 && 
+                 doc_top <= ori_elem_bottom + placeholder && 
+                 (elem_top == -elem_height || !elem.hasClass('elem_scroll_style1_on')) ){
                 elem.removeClass('elem_scroll_style1_on');
+                elem.css({'top': '0px'});
+                console.log("buffer area");
                 return;
             }
 
-            {   // relative
-                if ( !elem.hasClass('elem_scroll_style1_on') ){ // 變fixed前 預先移動到螢幕外
+            {   
+                // 變fixed前 預先移動到螢幕外
+                if ( !elem.hasClass('elem_scroll_style1_on') ){
                     elem.css({'top': (-elem_height)+'px'});
+                    console.log("pre_action for fixed");
                 }
-
-                if ( elem_top == -elem_height + original_elem_top ){ //fixed預先移動完成 即加上fixed
+                //fixed預先移動完成 即加上fixed
+//                console.log(elem_top , -elem_height + ori_elem_top);
+                if ( parseInt(elem_top) == parseInt(-elem_height + ori_elem_top) ){ 
+                    console.log("add fixed");
                     elem.addClass('elem_scroll_style1_on');
                 }
             }
 
             // 超出高度
             if ( dmdc > 50 ){
+            console.log(" show on top");
                 dmdc = 50;
                 elem.css({"top": "0px"});
             }
             if (dmdc < -50) {
+            console.log(" hide on top");
                 dmdc = -50;
                 elem.css({"top": (-elem_height)+"px"});
             }
